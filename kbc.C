47ec4106fa588b2eb1c2f2a1dd39c89093eb7b64
CO18353 <42859006+CO18353@users.noreply.github.com>
2018-08-31 18:58:20 +0530

    Add files via upload

---
+++ kbc.C
@@ -0,0 +1,494 @@
+#include<stdio.h>
+void main()
+{
+ 
+ int o,c,n,x,y,z,w;
+ w=0;x=0;y=0;z=0;
+ printf("Welcome to KBC!\n There are in total 15 questions and correctly answering all of them will earn you 1 crore rupees!!\n");
+
+ printf("Question 1 \n Which of the following country hosted the first Football World Cup? \n 1) America	2) Argentina \n 3) Brazil	4) Uruguay");
+ printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+ scanf("%d",&n);
+ switch(n)
+ {
+ case 1:printf("Which of the following country hosted the first Football World Cup? \n 1) 	2)  \n C. Brazil	4) Uruguay\n"); w++; break;
+ case 2:printf("1)-5	2)-5	3)-30	4)-60\n"); x++; break;
+ case 3:printf("Ans is 4\n"); y++; break;
+ case 4:printf("Call Tanveer"); z++; break;
+ default:break;
+ }
+ printf("\nEnter your choice\n"); 
+ scanf("%d",&c);
+ if(c==4)
+ {
+   printf("Absolutely correct ! You have won 1 thousand rupees !!\n");
+   printf("Question 2 \n Which country became the first nation to win the Football World Cup? \n 1) Uruguay	2) Germany \n 3) Argentina	4) Belgium");
+   printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+   scanf("%d",&n);
+   switch(n)
+ {
+
+ case 1:
+ (w==0?(printf("Which country became the first nation to win the Football World Cup? \n 1)Uruguay 	2)  \n 3)Argentina 	4) \n")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-50	2)-10	3)-30	4)-10\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 1\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+   printf("\nEnter your choice\n"); 
+   scanf("%d",&c);
+   if(c==1)
+   {
+     printf("Absolutely correct ! You have won 5 thousand rupees !!\n");
+     printf("Question 3 \n The international border between India-Pakistan along Punjab is known as which line? \n 1) Line of Control	2) Mannerheim Line \n 3) McMahon Line 	4) Radcliffe Line");
+     printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("The international border between India-Pakistan along Punjab is known as which line? \n 1) Line of Control	2)  \n 3) 	4) Radcliffe Line")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-10	2)-10	3)-30	4)-50\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 4\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+     printf("\nEnter your choice\n"); 
+     scanf("%d",&c);
+     if(c==4)
+     {
+       printf("Absolutely correct ! You have won 10 thousand rupees !!\n");
+       printf("Question 4 \n Which of the following is the largest railway junctions of Punjab? \n 1) Jalandhar Cantt	2) Bathinda \n 3) Ludhiana	4) Pathankot");
+       printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("1 or 2")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-10	2)-60	3)-30	4)-0\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 2\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+       printf("\nEnter your choice\n"); 
+       scanf("%d",&c);
+       if(c==2)
+       {
+         printf("Absolutely correct ! You have won 20 thousand rupees !!\n");	
+	 printf("Question 5 \n  Who among the following scored the first goal in World Cup history? \n 1) Johino	2) Bert Patenaude  \n 3) Lucien Laurent	4) Pele");
+         printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("3 or 4")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-10	2)-10	3)-50	4)-30\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 3\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+ 	 printf("\nEnter your choice\n"); 
+	 scanf("%d",&c);
+	 if(c==3)
+	 {
+	   printf("Absolutely correct ! You have won 55 thousand rupees !!\n");
+           printf("Question 6 \n  Who sang the song: Wavin Flag? \n 1) Drake	2) K'Naan  \n 3) Louis 	4) Bruno Mars");
+           printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("2 or 4")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-20	2)-31	3)-20	4)-29\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 2\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+	   printf("\nEnter your choice\n"); 	
+           scanf("%d",&c);
+	   if(c==2)
+           {
+             printf("Absolutely correct ! You have won 70 thousand rupees !!\n"); 
+ 	     printf("Question 7 \n Which of the following is the poorest conductor of heat in comparison to other options? \n 1) Silver	2) Copper \n 3) Lead	4) Mercury");
+             printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("3 or 4")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-10	2)-10	3)-50	4)-30\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 3\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+ 	     printf("\nEnter your choice\n"); 
+ 	     scanf("%d",&c);
+ 	     if(c==3)
+ 	     {
+   	       printf("Absolutely correct ! You have won 85 thousand rupees !!\n");
+               printf("Question 8 \n Which of the following organizations creates the topographical maps of India ? \n 1) Geological survey of India 	2) Survey of India \n 3) Geographical Survey of India	4) None of these");
+printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("3 or 2")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-10	2)-35	3)-25	4)-30\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 2\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+	       printf("\nEnter your choice\n");  
+   	       scanf("%d",&c);
+   	       if(c==2)
+               {
+	         printf("Absolutely correct ! You have won 1 lakh rupees !!\n");
+    		 printf("Question 9 \n '.MOV' extension refers usually to what kind of file? \n 1) Image file	2) Animation/Movie File \n 3) MS Office document 	4) Music file");
+		 printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("3 or 2")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-10	2)-38	3)-22	4)-30\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 2\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+ 		 printf("\nEnter your choice\n");  
+		 scanf("%d",&c);
+		 if(c==2)
+		 { 
+                   printf("Absolutely correct ! You have won 9 lakh rupees !!\n");
+	 	   printf("Question 10 \n Which is a type of Electrically-Erasable Programmable Read-Only Memory? \n 1) Flash	2) NTFS \n 3) FRAM	4) RAW");
+		   printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("1 or 4")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-50	2)-10	3)-10	4)-30\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 1\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+		   printf("\nEnter your choice\n"); 
+		   scanf("%d",&c);
+		   if(c==1)
+		   {
+                     printf("Absolutely correct ! You have won 20 lakh rupees !!\n"); 
+		     
+	             
+       	             printf("Question 11 \n  Who has become the first-ever Indian shuttler to enter a badminton final at the Asian Games? \n 1) PC Thulasi	2) Saina Nehwal  \n 3) PV Sindhu	4) Kidambi Srikanth");
+		     printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("3 or 2")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-10	2)-10	3)-50	4)-30\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 3\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+		     printf("\nEnter your choice\n"); 
+		     scanf("%d",&c);
+	             if(c==3)
+		     {
+                       
+	               
+	               printf("Question 12 \n  Who is the president of North Korea? \n 1) Xi Jinping	2) Kim Jong Un  \n 3) Donald Trump 	4) Moon Jae In");
+		       printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("2 or 4")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-10	2)-40	3)-20	4)-30\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 2\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+		       printf("\nEnter your choice\n"); 	
+ 		       scanf("%d",&c);
+		       if(c==2)
+ 		       {
+                         
+			 printf("Question 13 \n The Hand of God goal was scored by whom? \n 1) Diego Armando Maradona	2) Pele \n 3) Lionel Messi	4) Louis Suarez");
+		         printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+ 
+ case 1:
+ (w==0?(printf("4 or 2")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-90	2)-10	3)-0	4)-0\n")):(printf("Lifeline is already used"))); 
+ x++; 
+ break;
+ case 3:
+ (y==0?(printf("Ans is 1\n")):(printf("Lifeline is already used"))); 
+ y++; 
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+			 printf("\nEnter your choice\n"); 
+		         scanf("%d",&c); 
+			 if(c==1)
+			 {
+			   printf("Absolutely correct ! You have won 80 lakh rupees !!\n");
+			   printf("Do you wish to continue?\n"); 
+			   scanf("%d",&o);
+			   if(o==1)
+			   {
+			   printf("Question 14 \n What are a group of Dolphins called? \n 1) Pod	2) School \n 3) Pool	4) Herd");
+			   printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+
+ case 1:
+ (w==0?(printf("3 or 1")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-40	2)-10	3)-20	4)-30\n")):(printf("Lifeline is already used")));
+ x++;
+ break;
+ case 3:
+ (y==0?(printf("Ans is 1\n")):(printf("Lifeline is already used")));
+ y++;
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+			   printf("\nEnter your choice\n"); 
+			   scanf("%d",&c);
+			   if(c==1)
+			   {
+			     printf("Absolutely correct ! You have won 90 lakh rupees !!\n");
+			     printf("Do you wish to continue?\n"); 
+			     scanf("%d",&o);
+			     if(o==1)
+			     {
+			     printf("Question 15 \n What is the currency of Brazil?\n 1) The Bath	2) Krona \n 3) Rand 	 4) Real");
+			     printf("\nEnter:	 1.50/50\n2.Audience Poll\n3.Ask the Expert\n4.Phone a Friend\n5.None\n");
+     scanf("%d",&n);
+     switch(n)
+ {
+
+ case 1:
+ (w==0?(printf("3 or 4")):(printf("Lifeline already Used")));
+ w++;
+ break;
+ case 2:
+ (x==0?(printf("1)-10	2)-10	3)-30	4)-50\n")):(printf("Lifeline is already used")));
+ x++;
+ break;
+ case 3:
+ (y==0?(printf("Ans is 4\n")):(printf("Lifeline is already used")));
+ y++;
+ break;
+ case 4:
+ (z==0?(printf("Call Tanveer")):(printf("Lifeline is already used")));
+ z++;
+ break;
+ default:break;
+ }
+			     printf("\nEnter your choice\n"); 
+			     scanf("%d",&c);
+		 	     if(c==4)
+			     printf("Absolutely correct ! You have won 1 crore rupees !!\n");
+                             
+			     else
+                             printf("Alas ! you are wrong. The correct answer is 4. You loose everything\n");
+			     }
+			     else if(o==0)
+			     printf("You played very well and won 90 lakh rupees");	 
+			   }
+			   else
+                           printf("Alas ! you are wrong. The correct answer is 1. You loose everything\n");
+			  }
+                          else if(o==0)
+			  printf("You played very well and won 80 lakh rupees");	 
+			}
+			           else
+                                   printf("Alas ! you are wrong. The correct answer is 3) You loose everything\n");
+	                          }
+		                 else
+                                 printf("Alas ! you are wrong. The correct answer is 2. You loose everything\n");
+				}
+                                else
+                                printf("Alas ! you are wrong. The correct answer is 3. You loose everything\n");
+                              }
+                             else
+                             printf("Alas ! you are wrong. The correct answer is 1. You loose everything\n");
+                            }
+                           else
+                           printf("Alas ! you are wrong. The correct answer is 2. You loose everything\n");
+                          }
+                         else
+                         printf("Alas ! you are wrong. The correct answer is 2. You loose everything\n");
+                        }
+                       else
+                       printf("Alas ! you are wrong. The correct answer is 3. You loose everything\n");
+                      }
+                     else
+                     printf("Alas ! you are wrong. The correct answer is 2. You loose everything\n");
+                    } 
+                   else
+                   printf("Alas ! you are wrong. The correct answer is 3. You loose everything\n");
+                  }
+                 else
+                 printf("Alas ! you are wrong. The correct answer is 2. You loose everything\n");
+                }
+               else
+               printf("Alas ! you are wrong. The correct answer is 4. You loose everything\n");
+              }	
+             else
+             printf("Alas ! you are wrong. The correct answer is 1. You loose everything\n");
+            }
+           else
+           printf("Alas ! you are wrong. The correct answer is 4. You loose everything\n");
+}	
+
+				
